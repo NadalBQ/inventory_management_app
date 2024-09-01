24f6d80e05ee0fe8314665145f716698e7dc493a
NadalBQ <nadalbq@gmail.com>
2024-09-01 19:48:08 +0200

    added the addresses for the web pages themselves

--- app.py
+++ app.py
@@ -4,6 +4,46 @@ import pandas as pd
 
 
 app = Flask(__name__, static_url_path='/static')
+# Web addresses
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+@app.route('/home')
+def home():
+    return render_template('index.html')
+@app.route('/explain')
+def explain():
+    return render_template('explain.html')
+@app.route('/aboutus')
+def aboutus():
+    return render_template('aboutus.html')
+@app.route('/filesUsed')
+def filesUsed():
+    return render_template('filesUsed.html')
+@app.route('/theProject')
+def theProject():
+    return render_template('theProject.html')
+
+@app.route('/isabelle')
+def isabelle():
+    return render_template('isabelle.html')
+@app.route('/nadal')
+def nadal():
+    return render_template('nadal.html')
+@app.route('/fernanda')
+def fernanda():
+    return render_template('fernanda.html')
+@app.route('/carla')
+def carla():
+    return render_template('carla.html')
+@app.route('/michele')
+def michele():
+    return render_template('michele.html')
+
+
+
 
 @app.route('/add_item', methods=['POST'])
 
