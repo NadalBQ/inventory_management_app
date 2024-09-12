df4b2c5a3a9929d69350ff98898ff9e0330b4e4f
NadalBQ <nadalbq@gmail.com>
2024-09-12 12:19:08 +0200

    Updated the explain.html page

--- app.py
+++ app.py
@@ -23,28 +23,15 @@ def explain():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
-@app.route('/filesUsed')
-def filesUsed():
-    return render_template('filesUsed.html')
 @app.route('/theProject')
 def theProject():
     return render_template('theProject.html')
 
-@app.route('/isabelle')
-def isabelle():
-    return render_template('isabelle.html')
+
 @app.route('/nadal')
 def nadal():
     return render_template('nadal.html')
-@app.route('/fernanda')
-def fernanda():
-    return render_template('fernanda.html')
-@app.route('/carla')
-def carla():
-    return render_template('carla.html')
-@app.route('/michele')
-def michele():
-    return render_template('michele.html')
+
 
 
 
@@ -63,6 +50,7 @@ def add_item():
     Amount = str(data['amount'])
     Parent = str(data['parent'])
     Type = str(data['Type']).lower().capitalize()
+
     g = Github(token)
     repository = g.get_repo(repository_name)
     csv = repository.get_contents(csv_file_path)
